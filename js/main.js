b95c5e50872ba8165abea61830e5a13c8f421add
Mostafa Shaaban <mostafa.khalel.93@gmail.com>
2024-05-27 16:57:22 +0300

    add feature the sticky navbar and its functionality in js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ menuButton.addEventListener("click", () => {
     menuButton.classList.remove("open");
   }
 });
+
+const navBarElem = document.querySelector("main .container-fluid.sticky-top");
+const navBarBrand = document.querySelector("nav .navbar-brand");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navBarElem.classList.add("navbar-scrolled");
+    navBarBrand.style.color = "#777!important";
+  } else {
+    navBarElem.classList.remove("navbar-scrolled");
+    navBarBrand.style.color = "balck";
+  }
+});
